581c34b6b2949d0713c4072e419ee9d46a92f25e
vjsun-hub <vjsun@ucdavis.edu>
2026-02-05 22:15:20 -0800

    Add N to 1 multiplexer logic in muxNto1.sv
    Implemented basic functionality for N to 1 multiplexer.

--- Assignment1/muxNto1.sv
+++ Assignment1/muxNto1.sv
@@ -8,5 +8,14 @@ module muxNto1 #(
 );
     
     // TODO: Implement the N to 1 multiplexer logic
+    /*
+    The parameter N is defined as a parameter in the module. 
+    The width of each input and output is defined by the parameter WIDTH. 
+    The select signal will not select an input outside the range of N inputs.
+    */
+
+    always @(*) begin
+        out = in_bus[sel];
+    end
     
 endmodule

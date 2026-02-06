04c12b12b9b34a57301e1916ffe4e87d603a7199
vjsun-hub <vjsun@ucdavis.edu>
2026-02-05 22:18:04 -0800

    Add vending machine logic in SystemVerilog
    Implemented a Mealy model for a vending machine that accepts nickels and dimes, dispensing products and returning change as necessary.

--- Assignment1/vending_machine.sv
+++ Assignment1/vending_machine.sv
@@ -8,5 +8,78 @@ module vending_machine (
 );
 
     // TODO: Implement the vending machine logic here
+    /*
+    Implement a mealy model of a vending machine. 
+    The vending machine accepts nickel and dime only. 
+    When a total of 30 cents is inserted, the machine dispenses 
+    a product and returns any change if necessary.
+    */
+
+    localparam [2:0] 
+        S0 = 3'd0, // 0 cents
+        S5 = 3'd1, // 5 cents
+        S10 = 3'd2, 
+        S15 = 3'd3, 
+        S20 = 3'd4, 
+        S25 = 3'd5;
+
+    logic [2:0] curr_state, next_state;
+
+    always @(*) begin
+        next_state = curr_state;
+        dispense = 1'b0;
+        change = 1'b0; //equal to 0
+
+        case (curr_state)
+            S0: begin
+                if (nickel) next_state = S5;
+                else if (dime) next_state = S10;
+            end
+            S5: begin
+                if (nickel) next_state = S10;
+                else if (dime) next_state = S15;
+            end
+            S10: begin
+                if (nickel) next_state = S15;
+                else if (dime) next_state = S20;
+            end
+            S15: begin
+                if (nickel) next_state = S20;
+                else if (dime) next_state = S25;
+            end
+            S20: begin
+                if (nickel) next_state = S25;
+                else if (dime) begin
+                    dispense = 1'b1; //dispense the product, equal to 1
+                    next_state = S0;
+                end
+            end
+            S25: begin
+                if (nickel) begin
+                    dispense = 1'b1;
+                    next_state = S0;
+                end
+                else if (dime) begin
+                    dispense = 1'b1;
+                    change = 1'b1; //returns a nickel as change
+                    next_state = S0;
+                end
+            end
+
+            default: begin
+                next_state = S0;
+            end
+
+        endcase
+    end
+
+    //state transitions logic, for active low reset
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            curr_state <= S0;
+        end else begin
+            curr_state <= next_state;
+        end
+    end
 
 endmodule
